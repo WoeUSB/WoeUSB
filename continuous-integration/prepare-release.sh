528326c7d2ce10e3832a05fb63e55f3ad35e53bb
林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>
2020-11-01 22:32:55 +0800

    ci: Implement continuous delivery of standalone distribution
    Signed-off-by: 林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>

---
+++ continuous-integration/prepare-release.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+# Prepare release for upload
+# 林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>
+
+# Ensure script terminates when problems occurred
+set \
+    -o errexit \
+    -o nounset
+
+# apk add \
+#     git \
+#     sed
+
+script_dir="$(
+    realpath "${0%/*}"
+)"
+product_dir="$(
+    realpath \
+        "${script_dir}"/..
+)"
+git_describe="$(
+    git describe \
+        --always \
+        --tags \
+        --dirty
+)"
+product_version="${git_describe#v}"
+
+cp \
+    "${product_dir}"/sbin/woeusb \
+    "${product_dir}"/distribution/standalone/woeusb-standalone-"${product_version}"
+sed \
+    --regexp-extended \
+    --in-place \
+    "s/@@WOEUSB_VERSION@@/${product_version}/" \
+    "${product_dir}"/distribution/standalone/woeusb-standalone-"${product_version}"
---
+++ distribution/standalone/README.md
@@ -0,0 +1,13 @@
+# standalone
+
+The _standalone_ distribution ships a single Bash executable that can run independently.
+
+## Pros
+
+* Simply download and run, requires no installation
+
+## Cons
+
+* Does not ship dependencies and requires the user to have them installed independently
+* Cannot auto-update
+* (When implemented in the future) cannot support I18N
